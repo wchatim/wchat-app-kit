dd4c2051f804d2fdab0c4fcb219680852a1c125d
wchatim <wchatim@gmail.com>
2020-03-27 18:01:14 +0800

    去掉ijk，修改播放器

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ import QRCode from 'react-native-qrcode-svg';
 import Svg, {Circle,Ellipse,G,TSpan,TextPath,Path,Polygon,Polyline,Line,Rect,Use,Symbol,Defs,RadialGradient,Stop,ClipPath,Pattern,Mask} from 'react-native-svg';
 import LocalizedStrings from 'react-native-localization';
 import {setJSExceptionHandler, setNativeExceptionHandler} from 'react-native-exception-handler';
-import IJKPlayerView from "react-native-easy-ijkplayer";
 import RNIdle from 'react-native-idle';
+import {LivePlayer} from "react-native-dbb-rtmp";
 const {ScrollableTabBar} = ScrollableTabView;
 const {View,Image}  = RNKit;
 
@@ -40,7 +40,7 @@ module.exports = {
     LocalizedStrings,
     ScrollableTabView,
     ScrollableTabBar,
-    IJKPlayerView,
+    LivePlayer,
     RNIdle,
     Svg:{Svg,Circle,Ellipse,G,Text:Svg.Text,TSpan,TextPath,Path,Polygon,Polyline,Line,Rect,Use,Image:Svg.Image,Symbol,Defs,RadialGradient,Stop,ClipPath,Pattern,Mask}
 }
